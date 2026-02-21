276d345b921c9562cbaa614a31ad8a0c1ea63908
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2026-02-21 12:55:20 -0500

    refactor: Remove static from izzbuzz method

--- src/main/java/edu/eci/arcn/fizzbuzz/FizzBuzz.java
+++ src/main/java/edu/eci/arcn/fizzbuzz/FizzBuzz.java
@@ -23,7 +23,7 @@ public class FizzBuzz {
      * @param n the integer to evaluate
      * @return "FizzBuzz", "Fizz", "Buzz", or the number as a String
      */
-    public static String fizzbuzz(int n) {
+    public String fizzbuzz(int n) {
         if (n % 3 == 0 && n % 5 == 0) return "FizzBuzz";
         if (n % 3 == 0)               return "Fizz";
         if (n % 5 == 0)               return "Buzz";
